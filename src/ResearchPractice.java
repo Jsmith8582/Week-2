7a72f8329c6408b94c7bc5b155cb185d625282f1
Josh Smith <Jsmith8582@gmail.com>
2020-04-09 19:08:48 -0600

    Adding Coding exercises

--- src/ResearchPractice.java
+++ src/ResearchPractice.java
@@ -4,6 +4,23 @@ public class ResearchPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		for (int z = 0; z <= 20; z += 4) {
+			System.out.println(z);
+		}
+		
+//		int P = 16;
+//		while (P <=15) {
+//			
+//			System.out.println(P);
+//			P++;
+//		}
+//		do {
+//			System.out.println(P);
+//			P++;
+//		} while (P <= 15);
+//			
+//		
+		
 	}
 
 }
